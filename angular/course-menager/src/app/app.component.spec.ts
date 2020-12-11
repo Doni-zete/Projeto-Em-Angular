0fcc982a7997fd20d4fcff2d291f4affdad990a3
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-12-11 20:07:23 -0300

    atual

--- angular/course-menager/src/app/app.component.spec.ts
+++ angular/course-menager/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'course-manager'`, () => {
+  it(`should have as title 'course-menager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('course-manager');
+    expect(app.title).toEqual('course-menager');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('course-manager app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('course-menager app is running!');
   });
 });
