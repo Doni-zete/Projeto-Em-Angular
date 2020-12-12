07d3d177bcad06548e51f67771649013e05e95c1
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-12-12 20:59:08 -0300

    ajuste

--- angular/course-menager/src/app/courses/course-list.components.ts
+++ angular/course-menager/src/app/courses/course-list.components.ts
@@ -5,16 +5,18 @@ import { Course } from './course';
 @Component({
     selector: 'app-course-list',
     templateUrl: './course-list.components.html'
+    
 })
 export class CourseListComponent implements OnInit {
 
     courses: Course[] = [];
+
     ngOnInit(): void {
         this.courses = [
 {
             id: 1,
             name: 'Angular: Forms',
-            imageUrl: '',
+            imageUrl: '/assets/images/forms.png',
             price: 99.99,
             code: 'XPS-879',
             duration: 120,
@@ -24,20 +26,14 @@ export class CourseListComponent implements OnInit {
 {
             id: 2,
             name: 'Angular: Http',
-            imageUrl: '',
+            imageUrl: '/assets/images/http.png',
             price: 45.99,
             code: 'LXL-1034',
             duration: 80,
             rating: 4,
-            releaseDate: 'December, 11, 2020',
-
-
+            releaseDate: 'December, 21, 2020',
 }
     ]
 
-
-
     }
-
-
 }
